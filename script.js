c31abd7ff93bea68defadbe8090fd4c6a2b3a366
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-18 14:41:34 -0400

    Implemented check order functionality

--- script.js
+++ script.js
@@ -79,11 +79,26 @@ function dragDrop() {
 
 // Swap list items that are drag and drop
 function swapItems(fromIndex, toIndex) {
-    const itemOne = listItems[fromIndex].querySelector('.draggable');
-    const itemTwo = listItems[toIndex].querySelector('.draggable');
+    const itemOne = listItems[fromIndex].querySelector(".draggable");
+    const itemTwo = listItems[toIndex].querySelector(".draggable");
 
     listItems[fromIndex].appendChild(itemTwo);
     listItems[toIndex].appendChild(itemOne);
+}
+
+// Check the order of list items
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const personName = listItem.querySelector(".draggable").innerText.trim();
+
+        if (personName !== richestPeople[index]) {
+            listItem.classList.add("wrong");
+
+        } else {
+            listItem.classList.remove("wrong");
+            listItem.classList.add("right");
+        }
+    });
 }
 
 // Event Listeners
