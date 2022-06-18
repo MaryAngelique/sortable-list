8584e225b015d41b0a883381823483c8a8b676df
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-18 13:51:41 -0400

    Implemented event listeners

--- script.js
+++ script.js
@@ -35,15 +35,32 @@ function createList() {
         listItem.innerHTML = `
             <span class="number">${index + 1}</span>
             <div class="draggable" draggable="true">
-            <p class="person-name">${person}</p>
-            <i class="fas fa-grip-lines"></i>
-            </div>
+                <p class="person-name">${person}</p>
+                <i class="fas fa-grip-lines"></i>
+             </div>
         `;
 
         listItems.push(listItem);
 
         draggable_list.appendChild(listItem);
-        });
+    
+    });
 
     addEventListeners();
+}
+
+function addEventListeners() {
+    const draggables = document.querySelectorAll(".draggable");
+    const dragListItems = document.querySelectorAll(".draggable-list li");
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener("dragstart", dragStart);
+    });
+
+    dragListItems.forEach(item => {
+        item.addEventListener("dragover", dragOver);
+        item.addEventListener("drop", dragDrop);
+        item.addEventListener("dragenter", dragEnter);
+        item.addEventListener("dragleave", dragLeave);
+    });
 }
